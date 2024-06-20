78e6da02fcc1e2fb625fcb11d8d1f4548d76ca9f
Erik Skopp <skopp.erik@gmail.com>
2024-06-20 20:17:14 +0200

    Zip Hanlder

--- archiv/download.py
+++ archiv/download.py
@@ -62,7 +62,7 @@ def get_file_info(file_path):
     file_name = os.path.basename(file_path)
     file_extension = os.path.splitext(file_path)[1] if not os.path.isdir(file_path) else ""
     relative_path = os.path.relpath(file_path, start=os.path.join('archiv', 'static')).replace(os.sep, '/')
-    download_link = f"[Download](/static/{relative_path})"
+    download_link = f"[Download](/{relative_path})"
     sha256_hash = get_sha256(file_path)
     return file_name, last_edit, file_size_mb, file_extension, download_link, sha256_hash
 
